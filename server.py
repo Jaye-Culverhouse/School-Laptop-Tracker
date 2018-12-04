897f5512007303b36d2ff8c9f99422f725a7bbe2
Jade <jadehculverhouse@gmail.com>
2018-12-04 11:50:41 +0000

    Added database connection to main

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import libs.settings
 import libs.communication
+import sqlite3
 
 def main():
-	settingsObj = libs.settings.settingsManager("serverSettings.json")
-	pass
+	settingsObj = libs.settings.settingsManager("server_Settings.json")
+	
+	if not settingsObj.isSet("server_db_name"):
+		settingsObj.set("server_db_name", "server_db.db")
+		settingsObj.save()
+
+	db_connection = sqlite3.connect(settingsObj.get("server_db_name"))
+	
 
 if __name__ == '__main__':
 	main()
