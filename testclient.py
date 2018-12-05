62592ef4e68ac2b9e3f4991a596da3a09cf9c5b3
Jade <jadehculverhouse@gmail.com>
2018-12-05 14:00:17 +0000

    Changed test client

--- testclient.py
+++ testclient.py
@@ -3,9 +3,6 @@ from sys import argv
 import json
 s = socket.create_connection((argv[1],argv[2]))
 print(argv[1],argv[2])
-s.sendall(b'{"STATION_NUMBER": 1, "COMMAND_ID": 1, "MESSAGE": "", "REQUEST": "UPDATE", "ARGUMENTS": [], "PAYLOAD": {"testSetting": 1}}')
+s.sendall(b'{"STATION_NUMBER": 1, "COMMAND_ID": 1, "MESSAGE": "", "REQUEST": "GETNAME", "ARGUMENTS": [0], "PAYLOAD": {"testSetting": 1}}')
 s.sendall(b"\n")
-# print(s.recv(4096))
-k = s.recv(4096)
-k = json.loads(k)
-print(k["PAYLOAD"])
+print(s.recv(4096))
