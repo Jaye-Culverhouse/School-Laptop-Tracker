697c91945fa681a8a138221acaa35885e6addb46
Jade <jadehculverhouse@gmail.com>
2018-12-06 14:07:04 +0000

    added a function for communicating with server

--- client.py
+++ client.py
@@ -2,11 +2,43 @@ import pyforms
 from pyforms.basewidget import BaseWidget
 from pyforms.controls import ControlText
 from pyforms.controls import ControlButton
+
 import sys
+import socket
+import json
 
 import libs.client_cv
 import libs.DBIO
 import libs.settings
+import libs.communication
+
+
+### GLOBAL variables
+COUNTER = 0
+stations = {}
+database = None
+
+###End Global Variables
+
+def sendAndRecieve(command, settingsObj, COUNTER, database, stations):
+	
+	command = libs.DBIO.setCommandSettings(command, settingsObj, COUNTER)
+	command = json.dumps(command)
+	command = command + "\n\n"
+	command = command.encode("utf-8")
+	sock = socket.create_connection((settingsObj.get("server_hostname"),settingsObj.get("server_port")))
+	sock.sendall(command)	
+	recv = sock.recv(4096)
+	recv = recv.decode("utf-8")
+	recv = json.loads(recv)
+
+	# print(recv)
+
+	stations,database,settingsObj,response = libs.DBIO.getCorrectResponse(stations,database,settingsObj,recv)
+
+	COUNTER = COUNTER+1
+
+	return response, settingsObj, COUNTER, database, stations
 
 class CheckoutMain(BaseWidget):
 
@@ -24,11 +56,24 @@ class CheckoutMain(BaseWidget):
 
 def main():
 
+	###initialisation
+
+	global COUNTER, stations, database
+
 	settingsObj = libs.settings.settingsManager("settings/client_Settings.json")
 	
 	settingsObj = libs.settings.checkBasicSettings(settingsObj)
+	settingsObj.save()
 
-	pyforms.start_app(CheckoutMain)
+
+	online = libs.DBIO.createCommand({},MESSAGE="ONLINE")
+	response, settingsObj, COUNTER, database, stations = sendAndRecieve(online, settingsObj, COUNTER, database, stations)
+
+	
+	update = libs.DBIO.createCommand({}, REQUEST="UPDATE")
+	response, settingsObj, COUNTER, database, stations = sendAndRecieve(update, settingsObj, COUNTER, database, stations)
+	
+	# pyforms.start_app(CheckoutMain)
 
 if __name__ == '__main__':
 	main()
